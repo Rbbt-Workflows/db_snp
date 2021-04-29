933a0eda943a1ff73622f1e190bbf93266dec6cf
Miguel Vazquez <mikisvaz@gmail.com>
2021-04-29 17:29:49 +0200

    nil

--- workflow.rb
+++ workflow.rb
@@ -8,7 +8,7 @@ module DbSNP
     attr_accessor :organism
   end
 
-  self.organism = "Hsa/jan2013"
+  self.organism = "Hsa/feb2014"
 
   input :mutations, :array, "Genomic Mutation", nil, :stream => true
   input :by_position, :boolean, "Identify by position", false
